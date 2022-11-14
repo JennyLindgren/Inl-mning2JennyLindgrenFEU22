8d60b14d85d758509b5a71e65628e98ce8281114
Jenny Lindgren <jema.lindgren@gmail.com>
2022-11-14 14:50:19 +0100

    add more

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
- const ApiKey = "6a5f19d8f42a5b5921f5b030701c91eb"
+ // "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+ 
+const Url = "https://api.openweathermap.org/data/2.5/weather?"
+const getCord = `${Url}q=Helsingborg&appid=6a5f19d8f42a5b5921f5b030701c91eb`
+ 
+const ApiKey = "6a5f19d8f42a5b5921f5b030701c91eb"
+ 
+//Denna funktionen gör själva hämtningen och returnerar json
+async function fetchApi(url) {
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            console.log(response.ok)
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }
+        else{
+            console.log("Ett fel uppstod vid hämtningen");
+        }
+    }catch (error){
+        console.log(error);
+    }
+}
+ //Huvudfunktion hämtar först lon och lat utifrån stadsnamnet 
+async function getData() {
+    let lon, lat;
+    await fetchApi(getCord)
+        .then(response => {
+            lon = response.coord.lon;
+            lat = response.coord.lat;
+            console.log(response);
+        })
+        // Allt inom ` (backticks) översätts till strängvärde. Man använder det för att kunna lägga in variabler lätt
+    const weatherUrl = `${Url}lat=${lat}&lon=${lon}&units=metric&appid=${ApiKey}`;
+    console.log(weatherUrl);
+    //Hämtar vädret utifrån log och lat som hämtades ovanför
+    await fetchApi(weatherUrl)
+        .then(response => {
+            //Väljer id result 
+            const weatherOutput = document.querySelector("#result");
+            //Skapar en paragref
+            const paragraph = document.createElement("p");
+            //Lägger till paragrafen i #result
+            weatherOutput.append(paragraph);
+            //Skriver ut på webbläsaren
+            paragraph.innerHTML = `${Math.round(response.main.temp)} &#176;C ${response.weather[0].main}`
+        })
+
+}
+
+getData();
+ 
